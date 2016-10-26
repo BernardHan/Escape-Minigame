e6d79a17223c98c6026c3a7fb95628638cd6addb
Zhaoyang Han <bernardhan221@gmail.com>
2016-10-26 01:04:19 -0700

    initial commit

---
+++ Source/section3/section3GameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "section3.h"
+#include "section3GameMode.h"
+
+
+
+
---
+++ Source/section3/section3GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "section3GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SECTION3_API Asection3GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
